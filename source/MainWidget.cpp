30e45774e33261fd6a8cdc70ca1e1fe48fb3f523
adam-zhang <mingpingzhang@163.com>
2023-06-15 11:03:07 +0800

    added MainWidget as a main container for video and its constrols

---
+++ source/MainWidget.cpp
@@ -0,0 +1,14 @@
+/********************************************************************************
+> FileName:	MainWidget
+> Author:	Mingping Zhang
+> Email:	mingpingzhang@163.com
+> Create Time:	Thu Jun 15 11:01:35 2023
+********************************************************************************/
+
+#include "MainWidget.h"
+
+MainWidget::MainWidget(QWidget* parent)
+	:QWidget(parent)
+{}
+
+MainWidget::~MainWidget(){}
---
+++ source/MainWidget.h
@@ -0,0 +1,20 @@
+/********************************************************************************
+> FileName:	MainWidget
+> Author:	Mingping Zhang
+> Email:	mingpingzhang@163.com
+> Create Time:	Thu Jun 15 11:01:35 2023
+********************************************************************************/
+
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QWidget>
+
+class MainWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	MainWidget(QWidget* = nullptr);
+	~MainWidget();
+};
+#endif//MAINWIDGET_H
